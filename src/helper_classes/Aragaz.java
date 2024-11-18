b0b448c642a7b351c82e1d512661e280d5193031
Stf <bratescustef@gmail.com>
2024-11-18 22:14:49 +0200

    Change clasee si testare

--- src/helper_classes/Aragaz.java
+++ src/helper_classes/Aragaz.java
@@ -1,5 +1,55 @@
 package helper_classes;
 
-public class Aragaz {
-    
+public class Aragaz implements EchipamentBucatarie {
+    // Atribute specifice Aragaz
+    String marca;
+    String model;
+    double pret;
+    int numarArzatoare;
+    boolean aprindereElectrica;
+    String tipCombustibil; // Gaz, Electric, Mixt
+    boolean cuptorInclus;
+
+    // Constructor fara argumente
+    public Aragaz() {
+        this.marca = "Default";
+        this.model = "Standard";
+        this.pret = 1200.0;
+        this.numarArzatoare = 4;
+        this.aprindereElectrica = true;
+        this.tipCombustibil = "Gaz";
+        this.cuptorInclus = true;
+    }
+
+    // Constructor cu toate argumentele
+    public Aragaz(String marca, String model, double pret, int numarArzatoare,
+                  boolean aprindereElectrica, String tipCombustibil, boolean cuptorInclus) {
+        this.marca = marca;
+        this.model = model;
+        this.pret = pret;
+        this.numarArzatoare = numarArzatoare;
+        this.aprindereElectrica = aprindereElectrica;
+        this.tipCombustibil = tipCombustibil;
+        this.cuptorInclus = cuptorInclus;
+    }
+
+    // Implementare metoda turnOn
+    @Override
+    public void turnOn() {
+        System.out.println("Aragazul " + marca + " este acum pornit.");
+    }
+
+    // Implementare metoda turnOff
+    @Override
+    public void turnOff() {
+        System.out.println("Aragazul " + marca + " este acum oprit.");
+    }
+
+    // Implementare metoda toString
+    @Override
+    public String toString() {
+        return "Aragaz [Marca=" + marca + ", Model=" + model + ", Pret=" + pret +
+               ", Numar Arzatoare=" + numarArzatoare + ", Aprindere Electrica=" + aprindereElectrica +
+               ", Tip Combustibil=" + tipCombustibil + ", Cuptor Inclus=" + cuptorInclus + "]";
+    }
 }
