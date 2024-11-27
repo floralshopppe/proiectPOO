636d24f22422ffe6428e666970bcf0d552d9b07e
Stefan Alexandru <as659@student.ugal.ro>
2024-11-27 23:18:24 +0200

    Improved Congelator & LadaFrigo Tests

--- src/tests/CongelatorTestDrive.java
+++ src/tests/CongelatorTestDrive.java
@@ -10,27 +10,38 @@ public class CongelatorTestDrive {
         Congelator[] array = { congelator1, congelator2, congelator3 };
 
         printObjects(array);
+        System.out.println();
 
         for (int i = 0; i < array.length; i++) {
             System.out.println("#" + i + " ");
             array[i].turnOn();
         }
+        System.out.println();
 
         for (int i = 0; i < array.length; i++) {
             System.out.println("#" + i + " ");
             array[i].turnOff();
         }
+        System.out.println();
 
         System.out.println("#" + 3 + " ");
         congelator3.turnOn();
+        System.out.println();
 
         for (int i = 0; i < array.length; i++) {
             System.out.println("#" + i + " ");
             array[i].racireRapida();
         }
+        System.out.println();
+
+        Object[] congelatorArray = new Object[10];
+        for (int i = 0; i < congelatorArray.length; i++) {
+            congelatorArray[i] = new Congelator("Daewoo", i, i, i, i, "Alb", true, i);
+        }
+        printObjects(congelatorArray);
     }
 
-    public static void printObjects(Congelator[] array) {
+    public static void printObjects(Object[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.println("#" + i + " " + array[i].toString());
         }
--- src/tests/LadaFrigorificaTestDrive.java
+++ src/tests/LadaFrigorificaTestDrive.java
@@ -5,22 +5,33 @@ import helper_classes.*;
 public class LadaFrigorificaTestDrive {
     public static void main(String[] args) {
         LadaFrigorifica ladaFrigo1 = new LadaFrigorifica();
-        LadaFrigorifica ladaFrigo2 = new LadaFrigorifica("Samsung", 120, 200, 120, 160, "Alb", false,-4);
-        LadaFrigorifica ladaFrigo3 = new LadaFrigorifica("Daewoo", 100, 150, 100, 160, "Negru", true,2);
+        LadaFrigorifica ladaFrigo2 = new LadaFrigorifica("Samsung", 120, 200, 120, 160, "Alb", false, -4);
+        LadaFrigorifica ladaFrigo3 = new LadaFrigorifica("Daewoo", 100, 150, 100, 160, "Negru", true, 2);
         LadaFrigorifica[] array = { ladaFrigo1, ladaFrigo2, ladaFrigo3 };
         printObjects(array);
+        System.out.println();
+
         for (int i = 0; i < array.length; i++) {
             System.out.println("#" + i + " ");
             array[i].turnOn();
         }
+        System.out.println();
 
         for (int i = 0; i < array.length; i++) {
             System.out.println("#" + i + " ");
             array[i].turnOff();
         }
+        System.out.println();
+
+        Object[] LadaFrigoArray = new Object[10];
+        for (int i = 0; i < LadaFrigoArray.length; i++) {
+            LadaFrigoArray[i] = new LadaFrigorifica("Samsung", i, i, i, i, "Alb", false, i);
+
+        }
+        printObjects(LadaFrigoArray);
     }
 
-    public static void printObjects(LadaFrigorifica[] array) {
+    public static void printObjects(Object[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.println("#" + i + " " + array[i].toString());
         }
